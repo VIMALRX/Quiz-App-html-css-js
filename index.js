a7ed9100e3d1d05dcf26dbe19396bfd0539700a6
VIMALRX <vimalkannan624@gmail.com>
2024-08-13 11:02:59 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 function ans(id){
     if(id=="two"){
    const e=document.getElementById(id);
-    e.style.backgroundColor="green";}
+    e.style.backgroundColor="green";
+    // setTimeout(function(){
+    //     window.location.reload();
+    //   },500);
+          }
     else{
        const e=document.getElementById(id);
     e.style.backgroundColor="red";
+    setTimeout(function(){
+  window.location.reload();
+},200);
     }
     
 }
