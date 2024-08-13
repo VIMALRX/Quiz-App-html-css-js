51b139d154ec0d51de597d335ab7c481bb8e7711
VIMALRX <vimalkannan624@gmail.com>
2024-08-13 10:47:57 +0530

    create the file

---
+++ index.js
@@ -0,0 +1,10 @@
+function ans(id){
+    if(id=="two"){
+   const e=document.getElementById(id);
+    e.style.backgroundColor="green";}
+    else{
+       const e=document.getElementById(id);
+    e.style.backgroundColor="red";
+    }
+    
+}
